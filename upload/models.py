0e8042ca5cd71fcfcaa954bee5b529bd06fb459e
xlKrauser <diego.martinez.quevedo@unillanos.edu.co>
2022-01-06 19:57:10 -0500

    importe librerias en views.py

--- upload/models.py
+++ upload/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
 
 class Usuarios(models.Model):
-    archivo = models.FileField()
+    usuario = models.CharField(max_length=100)
+    clave = models.CharField(max_length=15)
+    
+
+
+
     
