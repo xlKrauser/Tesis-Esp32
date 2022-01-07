8b3c5094bf9e6087a24894e6d609465d31d0b465
xlKrauser <diego.martinez.quevedo@unillanos.edu.co>
2022-01-06 19:41:47 -0500

    Initial commit

---
+++ Subir/vistas.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http.request import HttpRequest
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+
+
+
+def cargar(request):
+    if request.method =='POST':
+        subir_archivo = request.FILES['archivo']
+        print(subir_archivo.name)
+        print(subir_archivo.size)
+    return render(request, 'cargar.html')
