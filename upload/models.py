7a7d4047f661f67585a0457b730f6e436647fc62
xlKrauser <95330813+xlKrauser@users.noreply.github.com>
2022-01-11 20:48:05 -0500

    Diego/test (#3)
    * importe librerias en views.py
    
    * Se elimino vistas.py

--- upload/models.py
+++ upload/models.py
@@ -1,13 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.fields import CharField
 
 # Create your models here.
 
 class Usuarios(models.Model):
-    usuario = models.CharField(max_length=100)
-    clave = models.CharField(max_length=15)
-    
-
-
-
-    
+    nombre =  models.CharField(max_length=100)
+    tamaño = models.IntegerField(max_length=20)
+    fecha =  models.DateTimeField('publicacion')
