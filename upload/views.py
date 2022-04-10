d3e8c8bb06fd77096d4580765affe8a7c3681e30
xlKrauser <diego.martinez.quevedo@unillanos.edu.co>
2022-04-09 20:17:18 -0500

    se elimino el archivo media/pruebas_servidor.ino.esp32.bin

--- upload/views.py
+++ upload/views.py
@@ -92,3 +92,19 @@ def cargar(request):
 
 def contacto(request):
     return render(request, 'contacto.html')
+=======
+<<<<<<< HEAD
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+
+def cargar(request):
+    if request.method =='POST':
+        subir_archivo = request.FILES['archivo']
+        fs = FileSystemStorage()
+        fs.save(subir_archivo.name, subir_archivo)
+    return render(request, 'cargar.html')
+
+
